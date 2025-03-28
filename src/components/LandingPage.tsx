beb131ac2cd90bfc09a491cfba4fe1dff1e47027
kdab <kdabiedeen1@gmail.com>
2025-03-28 17:13:51 -0400

    make page responsive

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { Box, Heading, Image } from "@chakra-ui/react";
+import { Box, Heading, Image, useBreakpointValue } from "@chakra-ui/react";
 
 // Chakra + Motion component
 const MotionHeading = motion(Heading);
@@ -12,32 +12,24 @@ const MotionBox = motion(Box);
 const commonHeadingStyle = {
 	textAlign: "center" as const,
 	fontWeight: "bold",
-	size: "6xl",
+	fontSize: { base: "3xl", md: "5xl", lg: "6xl" },
 } as const;
 
 const LandingPage = () => {
+	const headingY = useBreakpointValue({ base: -100, md: 0 });
+	const headingYOffset = useBreakpointValue({ base: 100, md: 100 });
+	console.log(headingY, headingYOffset);
+
 	return (
 		<Box
 			as="section"
 			className="landing-page"
 			position="relative"
 			minH="100vh"
-			pt="4rem"
-			overflow="hidden"
+			pt={{ base: "2rem", md: "4rem" }}
 		>
-			{/* Main Heading */}
-			<MotionHeading
-				as="h1"
-				{...commonHeadingStyle}
-				initial={{ opacity: 0.2, y: 550 }}
-				animate={{ opacity: 1, y: 650 }}
-				transition={{ duration: 3 }}
-			>
-				Speed dating optimized.
-			</MotionHeading>
-
 			<MotionBox
-				display="flex"
+				display={{ base: "block", md: "flex" }}
 				justifyContent="center"
 				overflow="hidden"
 				mb="1rem"
@@ -51,7 +43,7 @@ const LandingPage = () => {
 					src="/assets/speeddating1.jpg"
 					alt="Banner Left"
 					objectFit="cover"
-					w="50%"
+					w={{ base: "100%", md: "50%" }}
 					initial={{ x: "-100%", scale: 0.8 }}
 					animate={{ x: 0, scale: 1 }}
 					transition={{ duration: 0.8, ease: "easeOut" }}
@@ -62,20 +54,32 @@ const LandingPage = () => {
 					src="/assets/speeddating2.jpg"
 					alt="Banner Right"
 					objectFit="cover"
-					w="50%"
+					w={{ base: "100%", md: "50%" }}
 					initial={{ x: "100%", scale: 0.8 }}
 					animate={{ x: 0, scale: 1 }}
 					transition={{ duration: 0.8, ease: "easeOut" }}
 				/>
 			</MotionBox>
 
+			{/* Main Heading */}
+			<MotionHeading
+				as="h1"
+				{...commonHeadingStyle}
+				initial={{ opacity: 0.2, y: headingY }}
+				animate={{ opacity: 1, y: headingY + headingYOffset}}
+				transition={{ duration: 3 }}
+				viewport={{ once: true }}
+			>
+				Speed dating optimized.
+			</MotionHeading>
 			{/* Secondary Message - fixed near bottom of viewport */}
 			<MotionHeading
 				as="h1"
 				{...commonHeadingStyle}
-				initial={{ opacity: 0, y: 30 }}
-				animate={{ opacity: 1, y: 0 }}
+				initial={{ opacity: 0.2, y: headingY }}
+				animate={{ opacity: 1, y: headingY + headingYOffset + 30}}
 				transition={{ duration: 1, delay: 0.3 }}
+				viewport={{ once: true }}
 			>
 				Matching has never been easier.
 			</MotionHeading>
